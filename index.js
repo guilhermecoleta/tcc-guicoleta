970a1f750ec4ffb5a564824328ee5b91d8b73bad
Guilherme Coleta <guilhermepizzaia7@gmail.com>
2021-02-09 23:10:23 -0300

    teste'

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const restify = require('restify')
-const porta = 8080
-const servidor = restify.createServer()
-servidor.get('/', (req, res) => {
-  res.send('Ae! Servidor criado veio com resposta para o path /.')
-})
-servidor.listen(porta, () => {
-  console.log(`Servidor de pé em http://localhost:${porta}`)
-  console.log('Pra derrubar o servidor: ctrl + c')
-})
+const http = require('http');
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
